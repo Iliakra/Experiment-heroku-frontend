e96cce233172e426f952ecf9d920b5fdd8820c28
Iliakra <kravus27@gmail.com>
2021-06-02 15:29:48 +0300

    ede

--- index.js
+++ index.js
@@ -1,73 +1,79 @@
-function domBuild (tickets) {
-    let ticketsContainer = document.getElementsByClassName('tickets-container')[0];
-    let cancelAddingButton = document.getElementsByClassName('cancel-adding-button')[0];
-    let cancelDeletionButton = document.getElementsByClassName('cancel-deletion-button')[0];
-    cancelAddingButton.addEventListener('click', addTicketDialogCloseHandler);
-    cancelDeletionButton.addEventListener('click', deleteTicketDialogCloseHandler);
-    for (let i=0; i<tickets.length; i++) {
+/* eslint-disable no-plusplus */
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-shadow */
+/* eslint-disable camelcase */
+/* eslint-disable no-console */
+/* eslint-disable no-restricted-globals */
+function domBuild(tickets) {
+  const ticketsContainer = document.getElementsByClassName('tickets-container')[0];
+  const cancelAddingButton = document.getElementsByClassName('cancel-adding-button')[0];
+  const cancelDeletionButton = document.getElementsByClassName('cancel-deletion-button')[0];
+  cancelAddingButton.addEventListener('click', addTicketDialogCloseHandler);
+  cancelDeletionButton.addEventListener('click', deleteTicketDialogCloseHandler);
+  for (let i = 0; i < tickets.length; i++) {
+    const ticketContainer = document.createElement('div');
+    ticketContainer.classList.add('ticket-container');
+    ticketContainer.setAttribute('data-id', tickets[i].id);
+    ticketContainer.addEventListener('click', showDescription_handler);
 
-        let ticketContainer = document.createElement('div');
-        ticketContainer.classList.add('ticket-container');
-        ticketContainer.setAttribute('data-id',tickets[i].id);
-        ticketContainer.addEventListener('click', showDescription_handler);
-
-        let statusIndicator = document.createElement('div');
-        statusIndicator.classList.add('circle');
-        if (tickets[i].status === "true") {
-            let readyStatusIcon = document.createElement('img');
-            readyStatusIcon.classList.add('ready-status-icon');
-            readyStatusIcon.src = 'images/ready-status-icon.png';
-            statusIndicator.appendChild(readyStatusIcon);
-        }
-        ticketContainer.appendChild(statusIndicator);
-
-        let textContainer = document.createElement('div');
-        textContainer.classList.add('ticket-text-container');
-
-        let ticketName = document.createElement('p');
-        ticketName.classList.add('ticket-name');
-        ticketName.textContent = tickets[i].name;
-
-        let descriptionContainer = document.createElement('p');
-        descriptionContainer.classList.add('description','invisible');
-        ticketContainer.appendChild(descriptionContainer);
-
-        if (tickets[i].hasOwnProperty('description')) {
-            descriptionContainer.classList.remove('invisible');
-            descriptionContainer.textContent = tickets[i].description;
-        }
-
-        textContainer.appendChild(ticketName);
-        textContainer.appendChild(descriptionContainer);
-
-        ticketContainer.appendChild(textContainer);
-
-        let dataTimeIndicator = document.createElement('p');
-        dataTimeIndicator.classList.add('data-time-indicator');
-        dataTimeIndicator.textContent = tickets[i].created;
-        ticketContainer.appendChild(dataTimeIndicator);
-
-        let buttonsContainer = document.createElement('div');
-        buttonsContainer.classList.add('buttons-container');
-        let changeButton = document.createElement('button');
-        let deleteButton = document.createElement('button');
-        deleteButton.textContent = 'X';
-        let changeButtonIcon = document.createElement('img');
-        changeButton.addEventListener('click', openChangeTicketDialog_handler);
-        changeButtonIcon.classList.add('pencil');
-        changeButtonIcon.src = 'images/pencil.jpg';
-        changeButton.appendChild(changeButtonIcon);
-        changeButton.classList.add('change-button', 'circle');
-        changeButton.setAttribute('data-id',tickets[i].id);
-        deleteButton.classList.add('delete-button', 'circle');
-        deleteButton.setAttribute('data-id',tickets[i].id);
-        deleteButton.addEventListener('click', openDeleteTicketDialog_handler);
-        buttonsContainer.appendChild(changeButton);
-        buttonsContainer.appendChild(deleteButton);
-        ticketContainer.appendChild(buttonsContainer);
-
-        ticketsContainer.appendChild(ticketContainer);
+    const statusIndicator = document.createElement('div');
+    statusIndicator.classList.add('circle');
+    if (tickets[i].status === 'true') {
+      const readyStatusIcon = document.createElement('img');
+      readyStatusIcon.classList.add('ready-status-icon');
+      readyStatusIcon.src = 'images/ready-status-icon.png';
+      statusIndicator.appendChild(readyStatusIcon);
     }
+    ticketContainer.appendChild(statusIndicator);
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('ticket-text-container');
+
+    const ticketName = document.createElement('p');
+    ticketName.classList.add('ticket-name');
+    ticketName.textContent = tickets[i].name;
+
+    const descriptionContainer = document.createElement('p');
+    descriptionContainer.classList.add('description', 'invisible');
+    ticketContainer.appendChild(descriptionContainer);
+
+    if (tickets[i].hasOwnProperty('description')) {
+      descriptionContainer.classList.remove('invisible');
+      descriptionContainer.textContent = tickets[i].description;
+    }
+
+    textContainer.appendChild(ticketName);
+    textContainer.appendChild(descriptionContainer);
+
+    ticketContainer.appendChild(textContainer);
+
+    const dataTimeIndicator = document.createElement('p');
+    dataTimeIndicator.classList.add('data-time-indicator');
+    dataTimeIndicator.textContent = tickets[i].created;
+    ticketContainer.appendChild(dataTimeIndicator);
+
+    const buttonsContainer = document.createElement('div');
+    buttonsContainer.classList.add('buttons-container');
+    const changeButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    const changeButtonIcon = document.createElement('img');
+    changeButton.addEventListener('click', openChangeTicketDialog_handler);
+    changeButtonIcon.classList.add('pencil');
+    changeButtonIcon.src = 'images/pencil.jpg';
+    changeButton.appendChild(changeButtonIcon);
+    changeButton.classList.add('change-button', 'circle');
+    changeButton.setAttribute('data-id', tickets[i].id);
+    deleteButton.classList.add('delete-button', 'circle');
+    deleteButton.setAttribute('data-id', tickets[i].id);
+    deleteButton.addEventListener('click', openDeleteTicketDialog_handler);
+    buttonsContainer.appendChild(changeButton);
+    buttonsContainer.appendChild(deleteButton);
+    ticketContainer.appendChild(buttonsContainer);
+
+    ticketsContainer.appendChild(ticketContainer);
+  }
 }
 
 const preloader = document.getElementsByClassName('donut')[0];
