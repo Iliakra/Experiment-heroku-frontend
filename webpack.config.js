f6d43270007a8bc187be3ff2e7178e4ba1056703
Iliakra <kravus27@gmail.com>
2021-06-02 15:08:24 +0300

    erret

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: '.index.js',
+  entry: 'index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js',
